bd4830a85dbb5170287afbf24328d84ed1beaa80
Bill Barge <wbarge@tblaw.com>
2025-05-13 08:46:12 -0700

    added Telerik to the Blazor app
    added Telerik to the Blazor app
    also added the root composition pattern and set-up the configuration to read from the app.settings file and the environment vars

--- UI/Contract.Blazor/Program.cs
+++ UI/Contract.Blazor/Program.cs
@@ -1,10 +1,20 @@
 using Contract.Blazor.Components;
+using Contract.Blazor.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.ConfigureDi(builder.Configuration);
+
+builder.Services.AddTelerikBlazor();
 
 var app = builder.Build();
 
---
+++ UI/Contract.Blazor/Utilities/RootComposition.cs
@@ -0,0 +1,9 @@
+﻿namespace Contract.Blazor.Utilities;
+
+public static class RootComposition
+{
+    public static void ConfigureDi(this IServiceCollection services, IConfiguration configuration)
+    {
+      
+    }
+}
